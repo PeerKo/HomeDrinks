115d47f2502951d89c7ae68584ffc9922debb34e
EduardRot <eduard.rotaermel@hotmail.de>
2016-06-08 13:56:59 +0200

    dsf

--- search.php
+++ search.php
@@ -1,14 +1,14 @@
 <?php
 mysql_connect("localhost","root","PASSWORT") or die ("Keine Verbindung moeglich");
-	mysql_select_db("Zutaten") or die ("Die Datenbank existiert nicht");
+	mysql_select_db("App_Data/packages/HomeDrinks_DB") or die ("Die Datenbank existiert nicht");
 	mysql_query("SET NAMES 'utf8'");
 	
-	$result = mysql_query("SELECT * FROM Name WHERE text LIKE '%" . $_GET['search'] . "%' LIMIT 5");
+	$result = mysql_query("SELECT * FROM Zutaten WHERE Name LIKE '%" . $_GET['search'] . "%' LIMIT 5");
 	while($row = mysql_fetch_object($result))
 	{
 		echo '
 ';
-		echo preg_replace('/(' . $_GET['search'] . ')/Usi', '<span class="result">\\1</span>', $row->text);
+		echo preg_replace('/(' . $_GET['search'] . ')/Usi', '<span class="result">\\1</span>', $row->Name);
 		echo '
 ';
 	}
